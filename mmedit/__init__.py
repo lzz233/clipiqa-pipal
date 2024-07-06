0a9b65a32bc56f718e5997627ee19312c65e03da
lzz233 <66113955+lzz233@users.noreply.github.com>
2024-07-06 20:20:27 +0800

    Add files via upload

--- mmedit/__init__.py
+++ mmedit/__init__.py
@@ -1,34 +1,27 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-import mmcv
+from .backbones import *  # noqa: F401, F403
+from .base import BaseModel
+from .builder import (build, build_backbone, build_component, build_loss,
+                      build_model)
+from .common import *  # noqa: F401, F403
+from .components import *  # noqa: F401, F403
+from .extractors import LTE, FeedbackHourglass
+from .inpaintors import (AOTInpaintor, DeepFillv1Inpaintor, GLInpaintor,
+                         OneStageInpaintor, PConvInpaintor, TwoStageInpaintor)
+from .losses import *  # noqa: F401, F403
+from .mattors import DIM, GCA, BaseMattor, IndexNet
+from .registry import BACKBONES, COMPONENTS, LOSSES, MODELS
+from .restorers import ESRGAN, SRGAN, BasicRestorer
+from .synthesizers import CycleGAN, Pix2Pix
+from .transformers import SearchTransformer
+from .video_interpolators import CAIN, FLAVR, BasicInterpolator
 
-from .version import __version__, version_info
-
-try:
-    from mmcv.utils import digit_version
-except ImportError:
-
-    def digit_version(version_str):
-        digit_ver = []
-        for x in version_str.split('.'):
-            if x.isdigit():
-                digit_ver.append(int(x))
-            elif x.find('rc') != -1:
-                patch_version = x.split('rc')
-                digit_ver.append(int(patch_version[0]) - 1)
-                digit_ver.append(int(patch_version[1]))
-        return digit_ver
-
-
-MMCV_MIN = '1.3.13'
-MMCV_MAX = '1.8'
-
-mmcv_min_version = digit_version(MMCV_MIN)
-mmcv_max_version = digit_version(MMCV_MAX)
-mmcv_version = digit_version(mmcv.__version__)
-
-
-assert (mmcv_min_version <= mmcv_version < mmcv_max_version), \
-    f'mmcv=={mmcv.__version__} is used but incompatible. ' \
-    f'Please install mmcv-full>={mmcv_min_version}, <={mmcv_max_version}.'
-
-__all__ = ['__version__', 'version_info']
+__all__ = [
+    'AOTInpaintor', 'BaseModel', 'BasicRestorer', 'OneStageInpaintor', 'build',
+    'build_backbone', 'build_component', 'build_loss', 'build_model',
+    'BACKBONES', 'COMPONENTS', 'LOSSES', 'BaseMattor', 'DIM', 'MODELS',
+    'GLInpaintor', 'PConvInpaintor', 'SRGAN', 'ESRGAN', 'GCA',
+    'TwoStageInpaintor', 'IndexNet', 'DeepFillv1Inpaintor', 'Pix2Pix',
+    'CycleGAN', 'SearchTransformer', 'LTE', 'FeedbackHourglass',
+    'BasicInterpolator', 'CAIN', 'FLAVR'
+]
